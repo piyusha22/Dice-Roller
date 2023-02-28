338991b9b3199d987da9108273582a22c01e800b
piyusha22 <gpiyusha19@tbc.edu.np>
2023-02-28 21:10:36 +0545

    Adopted good coding practices by cleaning, reformatting and commenting the code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.dice_roller_btn)
 
+        /**** Good coding practices ****/
         //rollButton.setOnClickListener { rollDice() }
 
         rollButton.setOnClickListener {
@@ -24,7 +29,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
