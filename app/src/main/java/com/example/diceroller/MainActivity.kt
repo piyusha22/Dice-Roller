d35878694e1664c0212defc79dbb73c8b312e9d7
piyusha22 <gpiyusha19@tbc.edu.np>
2023-02-28 21:08:00 +0545

    Logic added to the diceroller app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.dice_roller_btn)
+
+        //rollButton.setOnClickListener { rollDice() }
+
+        rollButton.setOnClickListener {
+
+            // val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            // toast.show()
+
+            // val resultTextView: TextView = findViewById(R.id.textView)
+            // resultTextView.text = "6"
+            rollDice()
+        }
+    }
+
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.dice_roller_count)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
