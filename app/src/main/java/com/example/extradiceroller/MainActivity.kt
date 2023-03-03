60ef5503823bcf0980a561f9e8cc6eef27ef0b1c
piyusha22 <gpiyusha19@tbc.edu.np>
2023-03-03 16:29:10 +0545

    Logics added to the dice roller app

--- app/src/main/java/com/example/extradiceroller/MainActivity.kt
+++ app/src/main/java/com/example/extradiceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.extradiceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollBtn : Button = findViewById(R.id.dice_roller_btn)
+        val rollCount1: TextView = findViewById(R.id.dice_roller_count1)
+        val rollCount2: TextView = findViewById(R.id.dice_roller_count2)
+        rollBtn.setOnClickListener{
+            val diceNo1 = (1..6).random()
+            rollCount1.text= "" + diceNo1
+            val diceNo2 = (1..6).random()
+            rollCount2.text = ""+diceNo2
+        }
     }
 }
