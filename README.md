1ea739ed780601c658101cdb6c7be897dad8637a
piyusha22 <gpiyusha19@tbc.edu.np>
2023-03-10 23:54:53 +0545

    Modified the comments and README.md file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+# Developed by Piyusha Gurung, 77261101
+
+## Extra Dice Roller Application
+
+It is simple dice rolling application for Android where user can roll two dice and view the results on the screen.
+
+## Key Features of Extra Dice Roller Application :
+
+1. Rolling Dice
+2. Dice Result Display in two different Text Views
+
 ![](ExtraDiceRoller.gif)
--- app/src/main/java/com/example/extradiceroller/MainActivity.kt
+++ app/src/main/java/com/example/extradiceroller/MainActivity.kt
@@ -10,10 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** saves the reference to the Button and roll count object in a variable called rollBtn, rollCount1,
+         * rollCount2 respectively and finds them in the layout with its unique resource ID
+         */
         val rollBtn: Button = findViewById(R.id.dice_roller_btn)
         val rollCount1: TextView = findViewById(R.id.dice_roller_count1)
         val rollCount2: TextView = findViewById(R.id.dice_roller_count2)
+
+        /** set a click listener on rollButton object by calling the setOnClickListener() method */
         rollBtn.setOnClickListener {
+
+            /** Generates two random numbers between 1 and 6 (inclusive) representing dice rolls
+             * The first & second random number is displayed in a text view called
+             * rollCount1 and rollCount2 respectively
+             */
             val diceNo1 = (1..6).random()
             rollCount1.text = "" + diceNo1
             val diceNo2 = (1..6).random()
